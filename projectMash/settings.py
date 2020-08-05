7f1a39ae3a9bb4375cbd803103a1b4455d5fdcc6
Wang Yuran <yuranw3@illinois.edu>
2020-08-04 20:21:31 -0500

    add word-wrap to vote.html

--- projectMash/settings.py
+++ projectMash/settings.py
@@ -77,8 +77,8 @@ WSGI_APPLICATION = 'projectMash.wsgi.application'
 # settings.py
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'CS411'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
